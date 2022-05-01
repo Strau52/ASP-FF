4c253956798349db9b362b70567677b0947b7c5a
Strausz <strassergergo@gmail.com>
2022-05-01 19:43:26 +0200

    Added my javascript function

--- OGE3K6_Abstract_and_document_management/OGE3K6_Abstract_and_document_management/wwwroot/js/site.js
+++ OGE3K6_Abstract_and_document_management/OGE3K6_Abstract_and_document_management/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function confirmDelete(uniqId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqId;
+    var confirmDerleteSpan = 'confirmDeleteSpan_' + uniqId;
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDerleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDerleteSpan).hide();
+    }
+}
