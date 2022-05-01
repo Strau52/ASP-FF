ab0cc632c7f38f8d4648554a84df9b4d5a554d23
Strausz <strassergergo@gmail.com>
2022-05-01 18:35:24 +0200

    Added the site user model

---
+++ OGE3K6_Abstract_and_document_management/OGE3K6_Abstract_and_document_management/Models/SiteUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Abstract_and_document_management_website.Models
+{
+    public class SiteUser : IdentityUser
+    {
+        [StringLength(50)]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [StringLength(11)]
+        [Display(Name = "Seal number")]
+        public string SealNumber { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get { return $"{(Title != "" ? Title : "")} {LastName} {FirstName}"; }
+        }
+    }
+}
