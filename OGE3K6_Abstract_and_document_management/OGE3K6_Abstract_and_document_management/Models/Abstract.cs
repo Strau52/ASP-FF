ef93b4e79b7c48eebcc08cc00bdadf2123743d24
Strausz <strassergergo@gmail.com>
2022-05-01 18:35:18 +0200

    Added the abstract models

---
+++ OGE3K6_Abstract_and_document_management/OGE3K6_Abstract_and_document_management/Models/Abstract.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OGE3K6_Abstract_and_document_management.Models
+{
+    public class Abstract
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public string UID { get; set; }
+
+        [StringLength(150)]
+        [Required]
+        [Display(Name = "Title of the abstract")]
+        public string AbstractTitle { get; set; }
+
+        [StringLength(2500)]
+        [Required]
+        [Display(Name = "The abstract")]
+        public string AbstractText { get; set; }
+
+        [Display(Name = "Upload date")]
+        [Required]
+        public DateTime UploadTime { get; set; }
+
+        [Display(Name = "Uploader")]
+        [Required]
+        public virtual IdentityUser Uploader { get; set; }
+
+        [Display(Name = "Reviewer")]
+        public virtual IdentityUser Reviewer { get; set; }
+
+        [Display(Name = "Review date")]
+        public DateTime ReviewedAt { get; set; }
+
+        [Display(Name = "Status of the abstract")]
+        public AbstractStatus Status { get; set; }
+    }
+}
---
+++ OGE3K6_Abstract_and_document_management/OGE3K6_Abstract_and_document_management/Models/AbstractStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace OGE3K6_Abstract_and_document_management.Models
+{
+    public enum AbstractStatus
+    {
+        Uploaded,
+        Accepted,
+        Rejected,
+        ToBeAmmended
+    }
+}
