e2589da38d643561411f16b8a9fbbc6132f297cb
Strausz <strassergergo@gmail.com>
2022-05-01 18:27:39 +0200

    Added the base ASP.NET with MVC project

---
+++ OGE3K6_Abstract_and_document_management/OGE3K6_Abstract_and_document_management/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OGE3K6_Abstract_and_document_management.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ OGE3K6_Abstract_and_document_management/OGE3K6_Abstract_and_document_management/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OGE3K6_Abstract_and_document_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
